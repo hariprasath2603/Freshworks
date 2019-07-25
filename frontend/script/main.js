d237592cf20f40fd641be7a843be67ddeb5a340e
Hari Prasath <haripraath2603@gmail.com>
2019-07-25 08:58:00 +0530

    Documentatin for frontend maded
    i love this documentation

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -1,34 +1,34 @@
-var x;
-//localStorage.setItem(keyname, 20);
-function setVale(val){
-    x=val;
-    console.log(x);
-}
+
 function localVar(value){
-    localStorage.setItem("country", value);
-    widows.redirect()
+    localStorage.setItem("country", value);//
 }
+//this fuction called when the input box is updated 
 async function valueChange(value){
-    
-    value=document.getElementById("search").value;
-    if(value=="")
+    var x;    // to store data retrived from api
+    value=document.getElementById("search").value;  // getting value inside the text box
+    if(value=="") // if value is null then value is set to a because it shows null on passing empty string
     value='a';
     try{
-var b = await fetch("https://restcountries.eu/rest/v2/name/"+value).then(res=>res.json()).then(posts=>setVale(posts));    
+        //fetting data from API
+     var b = await fetch("https://restcountries.eu/rest/v2/name/"+value).then(res=>res.json()).then(posts=>x=posts);    
     }
     catch{
-        console.log("waiting");
+        //any error then updated here
+        document.getElementById("cardcontainer").innerHTML="<p>Sorry unable to load data. Please check internet connection</p>";
+        console.log("Error occured");
     }
-    console.log(b);
-    var cardcontainer=document.getElementById("cardcontainer");
- if(x.status==404){
-    cardcontainer.innerHTML="<h2>Sorry! No result Found</h2>";
- }   
- else{
-
-cardcontainer.innerHTML="";
-            for(var i=0;i<x.length && i<10 ;i++){
+    //console.log(b);
+    var cardcontainer=document.getElementById("cardcontainer");//getting container of cards
+            if(x.status==404){
+                //cecking status if value returnd is null then it was displayed
+                cardcontainer.innerHTML="<h2>Sorry! No result Found</h2>";
+            }   
+            else{
+                cardcontainer.innerHTML=""; //making card container epmty
+             for(var i=0;i<x.length && i<10 ;i++){//the card is restricted to 10 on considering perfomance on mind
+                //card ws creatd with flag, country name and  capital name
                 var temp="<a href='./detailed.html'><div class='card'onclick='localVar (\""+x[i].name+"\")'><img width='200px' height='150px'src="+x[i].flag+"></img><div class='ctry'>"+x[i].name+"</div><div class='cap'> Capital: "+x[i].capital+"</div></div></a>";
+                //card added to the cardcontainer
                 cardcontainer.innerHTML+=temp;
                 }
             }
--- frontend/script/maindet.js
+++ frontend/script/maindet.js
@@ -1,74 +1,92 @@
-var countyName=localStorage.getItem('country');
-document.title=countyName;
-valueChange();
+var countyName=localStorage.getItem('country'); //reading the respective  country name to be displayed
+document.title=countyName; //change the title of the page to countries name
+valueChange();      // caling the below function
+
+
 async function valueChange(){
-    
-var b = await fetch("https://restcountries.eu/rest/v2/name/"+countyName+"?fullText=true").then(res=>res.json()).then(posts=>x=posts);    
+ // fetching value of the particular country using the api   
+ try{
+    await fetch("https://restcountries.eu/rest/v2/name/"+countyName+"?fullText=true").then(res=>res.json()).then(posts=>x=posts);    
 
-var cardcontainer     =document.getElementById("root");
 
-//                var temp="<a href='./detailed.html'><div class='card'onclick='localVar (\""+x[0].name+"\")'><img width='200px' height='150px'src="+x[0].flag+"></img><div class='ctry'>"+x[0].name+"</div><div class='cap'> Capital: "+x[0].capital+"</div></div></a>";
+
+
                 
-                document.getElementById("cntryName").innerHTML=x[0].name;
-                document.getElementById("flag").setAttribute('src',x[0].flag);
-                document.getElementById("capital").innerText="Capital city : "+x[0].capital;
+                document.getElementById("cntryName").innerHTML=x[0].name; // setting country name
+                document.getElementById("flag").setAttribute('src',x[0].flag); // seting url for flag image
+                document.getElementById("capital").innerText="Capital city : "+x[0].capital; // country capital
+                // when more then then one values are in list format then this loop is executed 
                 var temp="";
-
                 for(var i=0;i<x[0].currencies.length;i++){
-                    temp+=x[0].currencies[i]["code"]+" - "+x[0].currencies[i]["name"]+" - "+x[0].currencies[i]["symbol"];
-                    console.log(temp);
+                    temp+=x[0].currencies[i]["code"]+" - "+x[0].currencies[i]["name"]+" - "+x[0].currencies[i]["symbol"]; // currency  contains three compinent- name,
+                    //console.log(temp);                                                                                    // symbol, representation of currency
                 }
-                document.getElementById("currencies").innerText=temp;
+                document.getElementById("currencies").innerText=temp; //setting currency 
+                // loading various name of the countery
                 temp=""; 
                 for(var i=0;i<x[0].altSpellings.length;i++){
                     temp+=x[0].altSpellings[i]+", ";
-                    console.log(temp);
+                    //console.log(temp);
                 }
-                document.getElementById("altrNam").innerText=" Alternative Names: "+temp.substr(0,temp.length-2);
-                document.getElementById("region").innerText=" Region : "+x[0].region;
-                
+                document.getElementById("altrNam").innerText=" Alternative Names: "+temp.substr(0,temp.length-2);//last toe didgit consist of ", " so deleted
+                document.getElementById("region").innerText=" Region : "+x[0].region;// region was updated
+                //neighbouring countries
                 temp=""; 
                 for(var i=0;i<x[0].borders.length;i++){
                     temp+=x[0].borders[i]+", ";
-                    console.log(temp);
+                    //console.log(temp);
                 }
-                document.getElementById("borders").innerText=temp.substr(0,temp.length-2);
+                document.getElementById("borders").innerText=temp.substr(0,temp.length-2);// updating neighbouring countries
+                
+                //loading various time zones
                 temp=""; 
                 for(var i=0;i<x[0].timezones.length;i++){
                     temp+=x[0].timezones[i]+", ";
-                    console.log(temp);
+                    //console.log(temp);
                 }
 
-                document.getElementById("timezone").innerText=temp;
+                document.getElementById("timezone").innerText=temp;//updating time zones
 
+                //languages used inside the country
                 temp=""; 
                 for(var i=0;i<x[0].languages.length;i++){
                     temp+=x[0].languages[i]["name"]+", ";
-                    console.log(temp);
+                    //console.log(temp);
                 }
 
-                document.getElementById("languages").innerText=temp.substr(0,temp.length-2);
-                document.getElementById("latlang").innerHTML="Latitude : "+x[0].latlng[0]+"<br>     Longditude : "+x[0].latlng[1];
-                document.getElementById("population").innerText="Population : "+x[0].population;
-                document.getElementById("area").innerHTML="Area : "+x[0].area+" m<sup>2</sup>";
+                document.getElementById("languages").innerText=temp.substr(0,temp.length-2);//languages updated
+
+                document.getElementById("latlang").innerHTML="Latitude : "+x[0].latlng[0]+"<br>     Longditude : "+x[0].latlng[1];//latitude and longditude are updated
+
+                document.getElementById("population").innerText="Population : "+x[0].population;// updating population of country
+                document.getElementById("area").innerHTML="Area : "+x[0].area+" m<sup>2</sup>";// updating area of country and adding m^2 to at last
+                
+                // organization in which the country part of 
                 temp=""; 
                 for(var i=0;i<x[0].regionalBlocs.length;i++){
                     temp+=x[0].regionalBlocs[0].name+"(" +x[0].regionalBlocs[0].acronym+"), ";
-                    console.log(temp);
+                    //console.log(temp);
                 }
-                document.getElementById("blocks").innerText=temp.substr(0,temp.length-2);
-                document.getElementById("call").innerText="Calling Codes: "+x[0].callingCodes;
+                document.getElementById("blocks").innerText=temp.substr(0,temp.length-2); // updating the organiztions
+
+                document.getElementById("call").innerText="Calling Codes: "+x[0].callingCodes;//updating the call codes of the country
+                //domains related to the country like .in, .us, .uk
                 temp=""; 
                 for(var i=0;i<x[0].topLevelDomain.length;i++){
                     temp+=x[0].topLevelDomain[i]+", ";
-                    console.log(temp);
+                    //console.log(temp);
                 }
 
 
-                document.getElementById("domain").innerText="Country Domains: "+temp.substr(0,temp.length-2);
-                document.getElementById("subregion").innerText="Subregion: "+x[0].subregion;
+                document.getElementById("domain").innerText="Country Domains: "+temp.substr(0,temp.length-2);// updating the domain's
+                document.getElementById("subregion").innerText="Subregion: "+x[0].subregion; //updating the suregions
                 
-                
+            }
+            catch{
+                //cation is displayed if any problem on connectio or on loading API
+                document.getElementById("root").innerHTML="<p>Sorry unable to load data. Please check internet connection</p>"
+                console.log("Problem on loading data");
+            }           
                 
                 }
             
